3cb69a6ac1499286600494ed888e3a3fa81a4fc4
ErdemTaha <80206110+Erdemtaha@users.noreply.github.com>
2022-07-28 16:56:11 +0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,18 +1,15 @@
 
 const items = [
-    { icon: 'fab fa-youtube', link: 'https://youtube.com/UmutBayraktarYT' },
-    { icon: 'fab fa-twitch', link: 'https://twitch.com/umutyzx' },
-    { icon: 'fab fa-discord', link: 'https://discord.com/users/615029465726320654' },
-    { icon: 'fab fa-instagram', link: 'https://instagram.com/umutxyp' },
-    { icon: 'fab fa-facebook', link: 'https://facebook.com/umutxyp' },
-    { icon: 'fab fa-github', link: 'https://github.com/umutxyp' },
+    { icon: 'fab fa-youtube', link: 'https://www.youtube.com/channel/UCPKJUziVe7O9-MQ8BWAMTWw' },
+    { icon: 'fab fa-discord', link: 'https://discord.gg/qE6DX3pbYP' },
+    { icon: 'fab fa-instagram', link: 'https://instagram.com/erdemincdy' },
   ]
 
 const navItems = [
     { icon: 'fal fa-home', active: 'fa fa-home', label: 'Home', href: '/' },
     { icon: 'fal fa-compass', active: 'fa fa-compass', label: 'Projects', href: '/projects' },
     { icon: 'fal fa-phone', active: 'fa fa-phone', label: 'Contact', href: '/contact' },
-    { icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Blogs', href: 'https://blog.umut.live' }
+    { icon: 'fal fa-rss', active: 'fa fa-rss', label: 'Blogs', href: 'https://blog.erdem.life' }
 ]
 
 import Link from 'next/link';
